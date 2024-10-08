528fdb9195da9219193da8d1c5b9467a85507f80
Víctor Henrique <103254170+ViktorHenrique@users.noreply.github.com>
2024-10-08 08:14:27 -0300

    first commit
    first commit

---
+++ src/main/java/br/gov/sp/fatec/projeto_spring2042/ProjetoSpring2042Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projeto_spring2042;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpring2042Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpring2042Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/projeto_spring2042/ProjetoSpring2042ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projeto_spring2042;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpring2042ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
